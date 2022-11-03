8e4f751ce1436648fbb2e4046ec4cdbf331afd26
yukonblue <77284557+yukonblue@users.noreply.github.com>
2022-11-03 09:01:43 -0400

    Establish `AVPlaybackKit` Swift package

---
+++ Sources/AVPlaybackKit/AVPlaybackKit.swift
@@ -0,0 +1,6 @@
+public struct AVPlaybackKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AVPlaybackKitTests/AVPlaybackKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AVPlaybackKit
+
+final class AVPlaybackKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AVPlaybackKit().text, "Hello, World!")
+    }
+}
